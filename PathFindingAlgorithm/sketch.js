9eebdfbb83346a0e59e9d192b27c0e220ed1672a
Habib Jinadu <adesolajinadu@gmail.com>
2020-12-19 20:01:36 -0700

    recommit: fixed bug with program choosing the longer path

--- PathFindingAlgorithm/sketch.js
+++ PathFindingAlgorithm/sketch.js
@@ -104,9 +104,9 @@ function setup (){
     
     
     start = grid[0][0]; // start spot is at position (0,0)
-    end = grid[1][10]; // end spot is at (cols-1, rows-20)
+    end = grid[17][19]; // end spot is at (cols-1, rows-20)
     
-    openSet.push(start);  // put the starting spot at the beginning of the open set
+    openSet.push(start);  // put the starting spot at the beginning of the open set.
     for (var i = 0; i < cols; i++)
     
         for (var j = 0; j < rows; j++)
